562cf9bebb18d053eaf17a1e82371f7d1a77f580
Ramy Hakam <pencilsoft1@gmail.com>
2023-09-01 21:12:46 +0200

    Fix  File Encoding Issue

--- src/Repository/Tenant/OrderRepository.php
+++ src/Repository/Tenant/OrderRepository.php
@@ -1,1 +1,16 @@
-<?phpnamespace App\Repository\Tenant;use App\Entity\Tenant\Order;use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;use Doctrine\Persistence\ManagerRegistry;class OrderRepository  extends ServiceEntityRepository{    public function __construct(ManagerRegistry $registry)    {        parent::__construct($registry, Order::class);    }}
+<?php
+
+namespace App\Repository\Tenant;
+
+use App\Entity\Tenant\Order;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class OrderRepository  extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Order::class);
+    }
+
+}
